328bdce2b7eebfa6edbf93eed7c6e004aa8020f3
Jesse <jesse@piascik.net>
2021-11-19 17:19:41 -0500

    Use generator for project

--- project.js
+++ project.js
@@ -1,24 +1,40 @@
 const Task = require('imdone-core/lib/task')
 
-module.exports = (function () {
-  let repo
+module.exports = function newProject(_repo) {
+  let repo = _repo
 
   class Project {
-    constructor(_repo) {
-      repo = _repo
+    get path () {
+      return repo.path
     }
 
     get doneList () {
       return repo.doneList
     }
 
-    addCardMetaData (card, key, value) {
+    get lists () {
+      return repo.getDefaultView().lists
+    }
+
+    get allLists () {
+      return repo.lists
+    }
+
+    get filter () {
+      return repo.filter
+    }
+
+    set filter (filter) {
+      repo.setFilter(filter)
+    }
+
+    addCardMetaData (task, key, value) {
       if (!/^['"]/.test(value) && /\s/.test(value)) value = `"${value}"`
       const metaData = `${key}${repo.config.getMetaSep()}${value}`
-      const content = Task.addToLastCommentInContent.apply(card, [card.content, metaData, repo.config.isMetaNewLine()])
-      repo.modifyTaskFromContent(card, content)
+      const content = Task.addToLastCommentInContent.apply(card, [task.content, metaData, repo.config.isMetaNewLine()])
+      repo.modifyTaskFromContent(task, content)
     }
   }
 
-  return Project;
-})();
+  return new Project(_repo);
+}
